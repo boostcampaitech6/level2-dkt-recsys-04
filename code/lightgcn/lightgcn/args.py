64323e13d2aa7e8dd996f53fd56b4bb34bfd4263
siyun <tldbs5026@naver.com>
2024-01-18 03:36:19 +0000

    🔨 add : add argument --purpose on lightgcn args.py

--- code/lightgcn/lightgcn/args.py
+++ code/lightgcn/lightgcn/args.py
@@ -1,13 +1,12 @@
 import argparse
 
-
 def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--seed", default=42, type=int, help="seed")
     parser.add_argument("--use_cuda_if_available", default=True, type=bool, help="Use GPU")
     
-    parser.add_argument("--data_dir", default="/opt/ml/input/data", type=str, help="")
+    parser.add_argument("--data_dir", default="/data/ephemeral/data/", type=str, help="")
     
     parser.add_argument("--output_dir", default="./outputs/", type=str, help="")
     
@@ -20,6 +19,9 @@ def parse_args():
     parser.add_argument("--model_dir", default="./models/", type=str, help="")
     parser.add_argument("--model_name", default="best_model.pt", type=str, help="")
     
+    # feat siyun
+    ## add argument --purpose
+    parser.add_argument('--purpose', choices = ['result','embedding'] , default = 'result' ,type=str, help = "lightgcn의 결과를 어떻게 사용할지 정합니다.")
 
     args = parser.parse_args()
 
